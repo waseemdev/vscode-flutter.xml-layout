27bbad0e772b7952bdbbe98097b4aeca41342daa
WaseemDev <waseemdev7@gmail.com>
2019-11-16 10:09:14 +0200

    fix hover bug

--- src/language-features/providers/dart_hover_provider.ts
+++ src/language-features/providers/dart_hover_provider.ts
@@ -17,9 +17,18 @@ export class DartHoverProvider implements HoverProvider {
 				return;
 			}
 			
-			let results: Hover[] = await commands.executeCommand('vscode.executeHoverProvider', dartDocument.uri, dartDocument.positionAt(dartOffset));
-			results.forEach(a => a.range = null);
-			return results[0];
+			const results: Hover[] = await commands.executeCommand('vscode.executeHoverProvider', dartDocument.uri, dartDocument.positionAt(dartOffset));
+			const contents: any[] = results && results[0] && results[0].contents;
+			if (!contents) {
+				return undefined;
+			}
+			// fixes bug that prevents the hover from showing
+			const hover = new Hover([
+				// { language: 'dart', value: (data.contents[0] as any).value },
+				(typeof contents[0] !== 'string' ? contents[0] && (contents[0] as any).value : contents[0]) || undefined,
+				(typeof contents[1] !== 'string' ? contents[1] && (contents[1] as any).value : contents[1]) || undefined
+			]);
+			return hover;
 		} catch (e) {
 			console.error(e);
 		}
